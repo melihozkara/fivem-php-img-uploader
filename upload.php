97c52aac7953da70c1c91bffe1f5d350740e668b
Melih Özkara <polygold64@gmail.com>
2020-06-01 02:30:59 +0300

    Yükleme klasör isimleri güncellendi
    Görsellerin yüklendiği klasörlerin isimleri otomatik tarih atanacak şekilde ayarlandı.

--- upload.php
+++ upload.php
@@ -1,26 +1,35 @@
 <?php
-    $dizin = 'img/';
-    $dosyaAdi = date("d-m-Y_H-i-s") . '_' . rand(0000000000,9999999999) . '.jpg';
-    $yuklenecek_dosya = $dizin . $dosyaAdi;
-    $url = "https://orneksite.com/img/"; // sonuna / koymanız gerekiyor.
+
+if(!file_exists(date("Y") . '/' . date("m"))) {
+    mkdir(date("Y") . '/' . date("m"));
+}
+if(!file_exists(date("Y") . '/' . date("m"). '/'. date("d"))) {
+    mkdir(date("Y") . '/' . date("m"). '/'. date("d"));
+}
+
+$dizin = date("Y") . '/' . date("m") . '/'. date("d") . '/';
+
+$dosyaAdi = date("H-i-s") . '_' . rand(0000000000,9999999999) . '.jpg';
+$yuklenecek_dosya = $dizin . $dosyaAdi;
+$url = "https://asd.com/" . $dizin; // sonuna / koymanız gerekiyor. -- you have to put / at the end.
 
 
-    if (move_uploaded_file($_FILES['files']['tmp_name'][0], $yuklenecek_dosya))
-    {
-        $result = [
-            "success" => true,
-            "files" => [
-                    [
-                        "hash" => $dosyaAdi,
-                        "name" => $dosyaAdi,
-                        "url" => $url . $dosyaAdi,
-                        "size" => $_FILES['files']['size'][0]
-                    ]
-            ],
+if (move_uploaded_file($_FILES['files']['tmp_name'][0], $yuklenecek_dosya))
+{
+    $result = [
+        "success" => true,
+        "files" => [
+                [
+                    "hash" => $dosyaAdi,
+                    "name" => $dosyaAdi,
+                    "url" => $url . $dosyaAdi,
+                    "size" => $_FILES['files']['size'][0]
+                ]
+        ],
 
-        ];
-        print_r(json_encode($result));
-    } else {
-        echo "Dosya yüklenemedi!\n";
-    }
+    ];
+    print_r(json_encode($result));
+} else {
+    echo "Dosya yüklenemedi!\n";
+}
 ?>
